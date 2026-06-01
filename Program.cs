fa67d59bfae670c42ad866da89ede602c65f79c3
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-01 10:03:47 +0200

    Console WriteLine and Write example

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Writing to console
+Console.WriteLine("A");
+// Wrinting to console on another line
+Console.WriteLine("B");
+// Write to console on the same line
+Console.Write("C");
+Console.WriteLine("D");
+
