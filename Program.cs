6bd456e465b01dc100ddf71321cf36bc2c78421f
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-01 11:11:46 +0200

    readLine and how to terminate an app

--- Program.cs
+++ Program.cs
@@ -1,22 +1,5 @@
-﻿// "assignment" operator is =
+﻿// read input from user
 
-// used to declare variables and assign values to them
-
-// in other languagues we can often just create a variable and make it be something
-// example:
-
-// to declare a variable, we use 3 things
-// 1. type (her its a string), a name (in camelCase usually), and then we use = (assignmnet operator), and a value;
-string greeting = "Hello"; 
-
-// create variable with users data (userName, userAge (years), userHeight (meters), userIsOnline (on/off))
-string userName = "Patryk";
-int userAge = 46;
-double userHeight = 1.8;
-bool userIsOnline = true;
-
-Console.WriteLine(greeting + " " + userName);
-
-Console.WriteLine("user age: " + userAge);
-Console.WriteLine("user height: " + userHeight);
-Console.WriteLine("user online? " + userIsOnline);
+Console.ReadLine(); // readline pauses code execution
+// to pass the line above hit "Enter"
+// or to terminate program hit "Ctrl-C"
