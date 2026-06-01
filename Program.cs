3ae748c48e2d3b067f97d6d5ed5c9d4853e8f63a
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-01 11:02:15 +0200

    int, double and bool example

--- Program.cs
+++ Program.cs
@@ -9,8 +9,14 @@
 // 1. type (her its a string), a name (in camelCase usually), and then we use = (assignmnet operator), and a value;
 string greeting = "Hello"; 
 
-// create a variable with users name
+// create variable with users data (userName, userAge (years), userHeight (meters), userIsOnline (on/off))
 string userName = "Patryk";
+int userAge = 46;
+double userHeight = 1.8;
+bool userIsOnline = true;
 
 Console.WriteLine(greeting + " " + userName);
 
+Console.WriteLine("user age: " + userAge);
+Console.WriteLine("user height: " + userHeight);
+Console.WriteLine("user online? " + userIsOnline);
