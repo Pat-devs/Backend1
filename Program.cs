5dd80163b248696949ccd826a8410167aee36d44
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-01 10:47:27 +0200

    using multiple string variables

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@
 // in other languagues we can often just create a variable and make it be something
 // example:
 
-var greeting = "Hello"; // var can be used to create a variable wihtout a type, but it should be avoided whenever possible!
+// to declare a variable, we use 3 things
+// 1. type (her its a string), a name (in camelCase usually), and then we use = (assignmnet operator), and a value;
+string greeting = "Hello"; 
 
-Console.WriteLine(greeting);
+// create a variable with users name
+string userName = "Patryk";
+
+Console.WriteLine(greeting + " " + userName);
 
