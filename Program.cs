4ab6cb9a5d26d2b2c6b6afa9a420f6a8433c2dc3
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-02 10:03:28 +0200

    modulo example

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿// On program start we show user a greeting or a menu
-Console.WriteLine("Welcome to our app!");
-Console.WriteLine();
+//Console.WriteLine("Welcome to our app!");
+//Console.WriteLine();
 
 // prompt user for a number
-Console.WriteLine("Enter your lucky number: ");
+//Console.WriteLine("Enter your lucky number: ");
 
-int luckyNumber = int.Parse(Console.ReadLine()); // ReadLine gir oss en String
+//int luckyNumber = int.Parse(Console.ReadLine()); // ReadLine gir oss en String
 
 
 // luckyNumber = luckyNumber + 2000; // same as: luckyNumber += 2000;
@@ -18,5 +18,28 @@ int luckyNumber = int.Parse(Console.ReadLine()); // ReadLine gir oss en String
 
 // luckyNumber = luckyNumber / 5; // same as: luckyNumber /= 2;
 
+// Modulo operator
+// Modulo is the remnainder after a division
 
-Console.WriteLine("luckyNumber is now " + luckyNumber);
+// examples:
+// 5 / 2 = 2 (med rest): 1
+// 4 / 2 = 2 (uten rest): 0
+// 3 / 2 = 2 (med rest): 1
+// 2 / 2 = 2 (uten rest): 0
+
+//int luckyNumber = 3 % 2;
+
+//Console.WriteLine("luckyNumber is now " + luckyNumber);
+
+int divisor = 2; // useful to find odd/even numbers
+
+Console.WriteLine(10 % divisor);
+Console.WriteLine(9 % divisor);
+Console.WriteLine(8 % divisor);
+Console.WriteLine(7 % divisor);
+Console.WriteLine(6 % divisor);
+Console.WriteLine(5 % divisor);
+Console.WriteLine(4 % divisor);
+Console.WriteLine(3 % divisor);
+Console.WriteLine(2 % divisor);
+Console.WriteLine(1 % divisor);
