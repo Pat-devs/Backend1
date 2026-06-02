d2a197dccc25f82b48765205933b6236e7083989
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-02 10:16:45 +0200

    simple calc start

--- Program.cs
+++ Program.cs
@@ -2,44 +2,16 @@
 //Console.WriteLine("Welcome to our app!");
 //Console.WriteLine();
 
-// prompt user for a number
-//Console.WriteLine("Enter your lucky number: ");
+// A simple calculator
+// 1. Greeting (tell user what they can do) 
+// 2. Ask user for first number
+// 3. Ask user for second number
+// 4. Show user the sum of the two numbers
 
-//int luckyNumber = int.Parse(Console.ReadLine()); // ReadLine gir oss en String
+Console.WriteLine("Sum of two numbers (a simple calculator)");
+Console.WriteLine("Enter the first number");
+// prompt user for the first number and store it in a variable
+int firstNumber = int.Parse(Console.ReadLine());
 
-
-// luckyNumber = luckyNumber + 2000; // same as: luckyNumber += 2000;
-
-// other math operators: -,*,/
-
-// luckyNumber = luckyNumber - 2; // same as: luckyNumber -= 2;
-
-//luckyNumber = luckyNumber * 5; // same as: luckyNumber *= 2;
-
-// luckyNumber = luckyNumber / 5; // same as: luckyNumber /= 2;
-
-// Modulo operator
-// Modulo is the remnainder after a division
-
-// examples:
-// 5 / 2 = 2 (med rest): 1
-// 4 / 2 = 2 (uten rest): 0
-// 3 / 2 = 2 (med rest): 1
-// 2 / 2 = 2 (uten rest): 0
-
-//int luckyNumber = 3 % 2;
-
-//Console.WriteLine("luckyNumber is now " + luckyNumber);
-
-int divisor = 2; // useful to find odd/even numbers
-
-Console.WriteLine(10 % divisor);
-Console.WriteLine(9 % divisor);
-Console.WriteLine(8 % divisor);
-Console.WriteLine(7 % divisor);
-Console.WriteLine(6 % divisor);
-Console.WriteLine(5 % divisor);
-Console.WriteLine(4 % divisor);
-Console.WriteLine(3 % divisor);
-Console.WriteLine(2 % divisor);
-Console.WriteLine(1 % divisor);
+// small check if it works :)
+Console.WriteLine("user entered: " + firstNumber);
