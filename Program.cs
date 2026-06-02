5860d7db1e5f34972b3598ef7969924de9f3d37c
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-02 10:23:08 +0200

    calculator: sum version

--- Program.cs
+++ Program.cs
@@ -9,9 +9,17 @@
 // 4. Show user the sum of the two numbers
 
 Console.WriteLine("Sum of two numbers (a simple calculator)");
-Console.WriteLine("Enter the first number");
+Console.WriteLine("Enter the first number: ");
 // prompt user for the first number and store it in a variable
 int firstNumber = int.Parse(Console.ReadLine());
 
-// small check if it works :)
-Console.WriteLine("user entered: " + firstNumber);
+Console.WriteLine("Enter the second number: ");
+// prompt user for the second number and store it in a variable
+int secondNumber = int.Parse(Console.ReadLine());
+
+// store the result (sum) of the two numbers
+int result = firstNumber + secondNumber; 
+
+// print the result for the user
+Console.WriteLine("The result is:");
+Console.WriteLine(firstNumber + " + " + secondNumber + " = " + result);
