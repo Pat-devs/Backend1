03cbda1befe0459caf71f82aeafa5d3be635f567
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-04 10:55:51 +0200

    implement greeting conditionals for all cases

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@
 // greeting based on time of day App
 // give a greeting if time of day is a number (0-23) or else tell user the time is invalid
 
-// if hour is >= 0 and <= 11 Say good night
-// if hour < 12 say "Good morning"
-// if hour > 12 say "Good afternoon"
-// if hour > 17 say "Good evening"
+// Pseudokode:
 
-int hour = 0;
+// if hour is outside of 0-23 range print invalid time
+// if hour is between 0 and 6 say "Good night"
+// if hour is between 7 and 11 say "Good morning"
+// if hour is between 12 and 17 say "Good afternoon"
+// if hour is between 18 and 23 say "Good evening"
+
+int hour = 999;
 
 // AND operator: &&
 // OR operator: ||
@@ -22,6 +25,20 @@ if (hour < 0 || hour > 23)
 else
 {
     // user entered a valid time
-    
-
+    if (hour <= 6)
+    {
+        Console.WriteLine("Good night");
+    }
+    else if (hour <= 11)
+    {
+        Console.WriteLine("Good morning");
+    }
+    else if (hour <= 17)
+    {
+        Console.WriteLine("Good afternoon");
+    }
+    else
+    {
+        Console.WriteLine("Good night");
+    }
 }
